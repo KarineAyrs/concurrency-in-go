4c654016a09a67c81a4d51e0edd17b1e29744528
Karine Ayrapetyants <kayarapetyanz@viasattech.ru>
2024-03-26 10:16:35 +0300

    section 2

---
+++ go.mod
@@ -0,0 +1,3 @@
+module ConcurrencyInGO
+
+go 1.21
---
+++ section-two/wait-groups/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func printSomething(s string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(s)
+}
+
+func main() {
+	var wg sync.WaitGroup
+	words := []string{
+		"alpha", "beta", "delta", "gamma", "pi", "zeta", "eta", "theta", "epsilon",
+	}
+
+	wg.Add(len(words))
+	for i, x := range words {
+		go printSomething(fmt.Sprintf("%d: %s", i, x), &wg)
+	}
+
+	wg.Wait()
+	wg.Add(1)
+	printSomething("Thi is the 2nd thing to be printed", &wg)
+}
